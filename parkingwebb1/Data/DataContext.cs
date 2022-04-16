874884557021d6434386494891e2ff0f2d805e01
JHOYMAR ALEXIS ISAZA LEZCANO <jhoymarisaza287673@correo.itm.edu.co>
2022-04-16 18:52:02 -0500

    migration initial

--- parkingwebb1/Data/DataContext.cs
+++ parkingwebb1/Data/DataContext.cs
@@ -3,13 +3,40 @@ using parkingwebb1.Data.Entities;
 
 namespace parkingwebb1.Data
 {
-    public class DataContext : DbContext 
+    public class DataContext : DbContext
     {
-        public DataContext(DbContextOptions<DbContext> options) : base(options)
+        public DataContext()
         {
         }
 
-        public DbSet<Country> Countries { get; set; }
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Country> Countries { get; set; }   
+
+       
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+
+            if (!options.IsConfigured)
+
+            {
+
+                options.UseSqlServer("A FALLBACK CONNECTION STRING");
+
+            }
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+
+        {
+
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Country>().HasIndex(c => c.Name).IsUnique();
+
+        }
+
     }
 }
-
